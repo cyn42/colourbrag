8205f954376f4e3af186252b3c635372cb773a83
Cynthia Taylor <cynthia.taylor@cantire.com>
2017-01-14 20:45:14 -0500

    Added palette component. Futzed with layout for far too long.

---
+++ app/components/palette/palette.component.js
@@ -0,0 +1,30 @@
+function PaletteController () {
+  var ctrl = this;
+
+  ctrl.colours = [
+        {
+          name: 'Olive',
+          colourname: "olive"
+        }, {
+          name: 'Olive Drab',
+          colourname: "olivedrab"
+        }, {
+          name: 'Dark Olive Green',
+          colourname: "darkolivegreen"
+        }, {
+          name: 'Forest Green',
+          colourname: "forestgreen"
+        }, {
+          name: 'Sienna',
+          colourname: "sienna"
+        }
+      ];
+
+}
+
+angular.
+  module("ColourBrag").
+  component('palette', {
+    templateUrl: 'app/components/palette/palette.template.html',
+    controller: PaletteController
+});
